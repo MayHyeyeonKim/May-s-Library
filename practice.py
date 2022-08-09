1878e794f679dab70f5dc5430d3bf24588b2017b
MayHyeyeonKim <110757357+MayHyeyeonKim@users.noreply.github.com>
2022-08-09 00:30:48 -0700

    I am a big girl now.

---
+++ practice.py
@@ -0,0 +1,26 @@
+from operator import not_
+import turtle
+
+
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(3+5)
+print(2*8)
+print(3*(3+1))
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+# 참/거짓
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+"우리 집 애완동물을 소개 합니다."
+animal = "강아지"
+name = "해피"
+
+print("우리집" +  animal + "의 이름은" + name + "입니다.")
