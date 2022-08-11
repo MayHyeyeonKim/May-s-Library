fd4c9b733ae14e61268157e8fd9129b81b055899
MayHyeyeonKim <110757357+MayHyeyeonKim@users.noreply.github.com>
2022-08-10 20:52:35 -0700

    Delete main-3.py

--- main-3.py
+++
@@ -1,6 +0,0 @@
-age = float(input())
-weight = float(input())
-heart_rate = float(input())
-time = float(input())
-calories = ((age * 0.2757) + (weight * 0.03295) + (heart_rate * 1.0781) - 75.4991) * (time / 8.368)
-print(f'Calories: {calories:.2f} calories')
